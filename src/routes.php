62615e57e046226296b189574da45890f84fbc73
vyashchuk@dunrose.ru <vyashchuk@dunrose.ru>
2024-05-19 20:51:36 +0300

    Added codesniffer and action

--- src/routes.php
+++ src/routes.php
@@ -4,101 +4,110 @@ use App\Database;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->get('/loans/all', function (Request $request, Response $response): Response {
-    $sql = "SELECT * FROM loans LIMIT 100";
+$app->get(
+    '/loans/all', function (Request $request, Response $response): Response {
+        $sql = "SELECT * FROM loans LIMIT 100";
 
-    try {
-        $db = new Database();
-        $conn = $db->connect();
-        $stmt = $conn->query($sql);
-        $loans = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
+        try {
+            $db = new Database();
+            $conn = $db->connect();
+            //        print_r($conn);
+            $stmt = $conn->query($sql);
+            $loans = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
 
-        $response->getBody()->write(json_encode($loans));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(200);
-    } catch (PDOException $e) {
-        $error = array(
+            $response->getBody()->write(json_encode($loans));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(200);
+        } catch (PDOException $e) {
+            $error = array(
             "message" => $e->getMessage()
-        );
+            );
 
-        $response->getBody()->write(json_encode($error));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(500);
+            $response->getBody()->write(json_encode($error));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(500);
+        }
     }
-});
+);
 
-$app->get('/loans/{id}', function (Request $request, Response $response): Response {
-    $id = $request->getAttribute('id');
+$app->get(
+    '/loans/{id}', function (Request $request, Response $response): Response {
+        $id = $request->getAttribute('id');
 
-    $sql = "SELECT * FROM loans WHERE id = $id";
+        $sql = "SELECT * FROM loans WHERE id = $id";
 
-    try {
-        $db = new Database();
-        $conn = $db->connect();
-        $stmt = $conn->query($sql);
-        $loan = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $db = null;
+        try {
+            $db = new Database();
+            $conn = $db->connect();
+            $stmt = $conn->query($sql);
+            $loan = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
 
-        $response->getBody()->write(json_encode($loan));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(200);
-    } catch (PDOException $e) {
-        $error = array(
+            $response->getBody()->write(json_encode($loan));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(200);
+        } catch (PDOException $e) {
+            $error = array(
             "message" => $e->getMessage()
-        );
+            );
 
-        $response->getBody()->write(json_encode($error));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(500);
+            $response->getBody()->write(json_encode($error));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(500);
+        }
     }
-});
+);
 
-$app->post('/loans/add', function (Request $request, Response $response, array $args): Response {
-    $data = $request->getParsedBody();
+$app->post(
+    '/loans/add',
+    function (Request $request, Response $response, array $args
+    ): Response {
+        $data = $request->getParsedBody();
 
-    $value = $data["value"];
-    $term = $data["term"];
-    $interest_rate = $data["interest_rate"];
+        $value = $data["value"];
+        $term = $data["term"];
+        $interest_rate = $data["interest_rate"];
 
-    $sql = "INSERT INTO loans (value, term, interest_rate) VALUES (:value, :term, :interest_rate);";
+        $sql = "INSERT INTO loans (value, term, interest_rate)
+                VALUES (:value, :term, :interest_rate);";
 
-    try {
-        $db = new Database();
-        $conn = $db->connect();
+        try {
+            $db = new Database();
+            $conn = $db->connect();
 
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':value', $value);
-        $stmt->bindParam(':term', $term);
-        $stmt->bindParam(':interest_rate', $interest_rate);
-        $stmt->execute();
-        $newId = $conn->lastInsertId();
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':value', $value);
+            $stmt->bindParam(':term', $term);
+            $stmt->bindParam(':interest_rate', $interest_rate);
+            $stmt->execute();
+            $newId = $conn->lastInsertId();
 
-        $db = null;
-        $response->getBody()->write(json_encode(['id' => $newId]));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(200);
-    } catch (PDOException $e) {
-        $error = array(
+            $db = null;
+            $response->getBody()->write(json_encode(['id' => $newId]));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(200);
+        } catch (PDOException $e) {
+            $error = array(
             "message" => $e->getMessage()
-        );
+            );
 
-        $response->getBody()->write(json_encode($error));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(500);
+            $response->getBody()->write(json_encode($error));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(500);
+        }
     }
-});
+);
 
 $app->put(
     '/loans/update/{id}',
-    function (Request $request, Response $response, array $args)
-    {
+    function (Request $request, Response $response, array $args) {
         $id = $request->getAttribute('id');
         $data = $request->getParsedBody();
         $amount_paid = $data['amount_paid'];
@@ -129,33 +138,37 @@ $app->put(
                 ->withHeader('content-type', 'application/json')
                 ->withStatus(500);
         }
-    });
-
-$app->delete('/loans/delete/{id}', function (Request $request, Response $response, array $args) {
-    $id = $args["id"];
-
-    $sql = "DELETE FROM loans WHERE id = $id";
-
-    try {
-        $db = new Database();
-        $conn = $db->connect();
-
-        $stmt = $conn->prepare($sql);
-        $result = $stmt->execute();
-
-        $db = null;
-        $response->getBody()->write(json_encode($result));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(200);
-    } catch (PDOException $e) {
-        $error = array(
-            "message" => $e->getMessage()
-        );
-
-        $response->getBody()->write(json_encode($error));
-        return $response
-            ->withHeader('content-type', 'application/json')
-            ->withStatus(500);
     }
-});
+);
+
+$app->delete(
+    '/loans/delete/{id}',
+    function (Request $request, Response $response, array $args) {
+        $id = $args["id"];
+
+        $sql = "DELETE FROM loans WHERE id = $id";
+
+        try {
+            $db = new Database();
+            $conn = $db->connect();
+
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute();
+
+            $db = null;
+            $response->getBody()->write(json_encode($result));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(200);
+        } catch (PDOException $e) {
+            $error = array(
+            "message" => $e->getMessage()
+            );
+
+            $response->getBody()->write(json_encode($error));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(500);
+        }
+    }
+);
